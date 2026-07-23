11a49a686ceb7eb1a1ba50c73290bb40c879c57a
Yehezkiel7166 <k11037166@gmail.com>
2026-07-23 06:48:12 +0000

    feat(brand): add brand ownership trait

---
+++ app/Domains/Brand/Concerns/BelongsToBrand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domains\Brand\Concerns;
+
+use App\Domains\Brand\Models\Brand;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait BelongsToBrand
+{
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
