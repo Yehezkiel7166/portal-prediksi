2ade8fe003d62d65dcf8638825c1652cd3710beb
Yehezkiel7166 <k11037166@gmail.com>
2026-07-23 06:14:01 +0000

    feat(brand): add brand initialization middleware

---
+++ app/Http/Middleware/InitializeBrandContext.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Domains\Brand\Support\BrandContextInitializer;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InitializeBrandContext
+{
+    public function __construct(
+        private readonly BrandContextInitializer $initializer,
+    ) {
+    }
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $this->initializer->initialize();
+
+        return $next($request);
+    }
+}
